bd3f8020205f40b2a5372d1986ebef725ec1926e
gabriel <gaby7dev@gmail.com>
2021-12-04 19:04:16 -0300

    ordenando el codigo

--- figuras-geometricas/figuras.js
+++ figuras-geometricas/figuras.js
@@ -3,12 +3,12 @@
 
 // Perímetro del cuadrado
 // L = lado
-function perimetroCuadrado(L){
+function perimetroCuadrado(L) {
   return L * 4;
 }
 
 // Área del cuadrado
-function areaCuadrado(L){
+function areaCuadrado(L) {
   return L * L;
 }
 
@@ -23,7 +23,7 @@ function perimetroTriangulo(A, B, C) {
 
 // área del triángulo
 // b = base y h = altura
-function areaTriangulo(b, h){
+function areaTriangulo(b, h) {
   return (b * h)/2;
 } 
 
@@ -31,7 +31,7 @@ function areaTriangulo(b, h){
 
 // Diámetro
 // r = Radio
-function diametroCirculo(r){
+function diametroCirculo(r) {
   return r * 2;
 }
 
@@ -39,20 +39,20 @@ function diametroCirculo(r){
 const PI = Math.PI;
 
 // Círcunferencia
-function circunferenciaCirculo(r){
+function circunferenciaCirculo(r) {
   const diametro = diametroCirculo(r);
   return diametro * PI;
 }
 
 // área
-function areaCirculo(r){
+function areaCirculo(r) {
   return (r*r) * PI;
 }
 
 // Capturar eventos de HTML
 
 // validar entradas
-function validarInput(input){
+function validarInput(input) {
     return ( Number(input) >= 1 && 
             !isNaN(input) && 
             input.length >= 1
@@ -61,66 +61,61 @@ function validarInput(input){
 }
 
 // perimetro cuadrado
-function calcularPerimetroCuadrado(){
+function calcularPerimetroCuadrado() {
+
   const resultado = document.querySelector(".resultado__perimetro--cuadrado");
   const L = document.getElementById("inputCuadrado").value;
-  if( validarInput(L) ){
+
+  if (validarInput(L)) {
     const perimetro = perimetroCuadrado( Number(L) )
     resultado.innerHTML = `Perímetro: ${perimetro} cm.` 
-  }
-  else
-    alert("Debes ingresar sólo números");
+  } else alert("Debes ingresar sólo números");
 }
 const btnCalcularPerimetroCuadrado = document.getElementById("calcularPerimetroCuadrado");
 btnCalcularPerimetroCuadrado.addEventListener("click", calcularPerimetroCuadrado);
 
 // área cuadrado
-function calcularAreaCuadrado(){
+function calcularAreaCuadrado() {
   const resultado = document.querySelector(".resultado__area--cuadrado");
   const L = document.getElementById("inputCuadrado").value;
 
-  if( validarInput(L) ){
+  if (validarInput(L)) {
     const area = areaCuadrado( Number(L) );
     resultado.innerHTML = `Área: ${area} cm².`;
-  }
-  else
-    alert("Debes ingresar sólo números");
+  } else alert("Debes ingresar sólo números");
 }
 const btnCalcularAreaCuadrado = document.getElementById("calcularAreaCuadrado");
 btnCalcularAreaCuadrado.addEventListener("click", calcularAreaCuadrado);
 
 
 //  Perímetro Triágulo
-function calcularPerimetroTriangulo(){
+function calcularPerimetroTriangulo() {
   const A = document.querySelector("#ladoA").value;
   const B = document.querySelector("#ladoB").value;
   const C = document.querySelector("#ladoC").value;
 
-  if( validarInput(A) && 
+  if (validarInput(A) && 
       validarInput(B) && 
-      validarInput(C) 
-  ){
+      validarInput(C)) {
+
     const resultado = document.querySelector(".resultado__perimetro--triangulo");
     const perimetro = perimetroTriangulo( Number(A), Number(B), Number(C) );
     resultado.innerHTML = `Perímetro: ${perimetro} cm.`
-  }
-  else
-    alert("Debes ingresar sólo números");
+
+  } else alert("Debes ingresar sólo números");
 }
 
 // Área Triágulo
-function calcularAreaTriangulo(){
+function calcularAreaTriangulo() {
   const b = document.getElementById("base").value;
   const h = document.getElementById("altura").value;
 
   
-  if( validarInput(b) && validarInput(h) ){
+  if (validarInput(b) && validarInput(h)) {
     const resultado = document.querySelector(".resultado__area--triangulo");
     const area = areaTriangulo( Number(b), Number(h) );
     resultado.innerHTML = `Área: ${area} cm².`;
-  }
-  else
-    alert("Debes ingresar sólo números");
+  } else alert("Debes ingresar sólo números");
 }
 
 const btnCalcularPerimetroTriangulo = document.querySelector("#calcularPerimetroTriangulo");
@@ -132,30 +127,26 @@ btnCalcularAreaTriangulo.addEventListener("click", calcularAreaTriangulo);
 
 
 //  Perímetro Circulo
-function calcularCircuferenciaCirculo(){
+function calcularCircuferenciaCirculo() {
   const r = document.querySelector("#radio").value;
 
-  if( validarInput(r) ){
+  if (validarInput(r)) {
     const resultado = document.querySelector(".resultado__perimetro--circulo");
     const perimetro = circunferenciaCirculo( Number(r) );
     resultado.innerHTML = `Circuferencia: ${perimetro} cm.`
 
-  }
-  else
-    alert("Debes ingresar sólo números");
+  } else alert("Debes ingresar sólo números");
 }
 
 // Área Circulo
-function calcularAreaCirculo(){
+function calcularAreaCirculo() {
   const r = document.querySelector("#radio").value;
   
-  if( validarInput(r) ){
+  if (validarInput(r)) {
     const resultado = document.querySelector(".resultado__area--circulo");
     const area = areaCirculo( Number(r) );
     resultado.innerHTML = `Área: ${area} cm².`;
-  }
-  else
-    alert("Debes ingresar sólo números");
+  } else alert("Debes ingresar sólo números");
 }
 
 const btnCalcularCircuferenciaCirculo = document.querySelector("#calcularCircuferenciaCirculo");
@@ -166,22 +157,21 @@ btnCalcularAreaCirculo.addEventListener("click", calcularAreaCirculo);
 
 // Triángulo isóseles altura
 
-function alturaTrianguloIsoseles(L1, L2, base){
+function alturaTrianguloIsoseles(L1, L2, base) {
   return Math.sqrt( 
     Math.pow(L1, 2) - Math.pow( (base/2) , 2)
   );
 }
 
-function calcularAlturaTriangulo(){
+function calcularAlturaTriangulo() {
   const lado1 = document.querySelector("#iso-lado-1").value;
   const lado2 = document.querySelector("#iso-lado-2").value;
   const base = document.querySelector("#iso-base").value;
 
-  if( validarInput(lado1) && 
+  if (validarInput(lado1) && 
       validarInput(lado2) && 
       validarInput(base) && 
-      lado1 === lado2 )
-  {
+      lado1 === lado2 ) {
 
     const resultado = document.querySelector(".resultado__altura-triangulo");
     const altura = alturaTrianguloIsoseles( 
@@ -189,11 +179,9 @@ function calcularAlturaTriangulo(){
       Number(lado2), 
       Number(base) 
     );
-    resultado.innerHTML = `Altura: ${altura} cm.`
 
-  }
-  else
-    alert("Debes ingresar sólo números y 2 lados iguales");
+    resultado.innerHTML = `Altura: ${altura} cm.`
+  } else alert("Debes ingresar sólo números y 2 lados iguales");
 }
 
 const bntCalcularAlturaTriangulo = document.querySelector("#calcularAlturaTriangulo");
